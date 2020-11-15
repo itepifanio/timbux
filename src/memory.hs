af52b999ccc8e5942850b24248282bf4b590cd18
Ítalo Epifânio <italoepifaniols@gmail.com>
2020-11-15 14:47:58 -0300

    Atualiza com a master. Modifica memory corrigindo as operações com pattern matching

--- src/memory.hs
+++ src/memory.hs
@@ -1,19 +1,12 @@
-module Memory where
+module Memory where      
 
--- import Lexer
--- import Token
+-- Definição do array
+-- Seja uma matriz identidade 2x2, sua representação será:
+-- MyArray [(MyInt 1,[0,0]),(MyInt 0,[0,1]),(MyInt 0,[1,0]),(MyInt 1,[1,1])] "nomeDaVariavel" "escopo" [2,2]
 
--- type MemoryCell = (Token,Token)
--- type Memory = [MemoryCell]       
-
--- essa definição de array seria algo como
--- meu_array = [7 8]
--- Array ([(7,[0 0]), (8, [0 1])], "escopo", "meu_array", 2, 1)
--- meu_array =  6 7
---              8 9 
--- Array ([(6, [0 0 0]), (7, [0 0 1]), (8, [1 0]), (9, [1 1])], "escopo", "meu_array", 2, 2)
--- Array [()]
--- | MyArray [(Type, [Int])] String String Int Int
+-- Definição dos demais tipos
+-- Seja um tipo inteiro, sua represetnação será:
+-- MyType (MyInt 1) "nomeDaVariavel" "escopo"
 
 data Typex = MyInt Int       |
              MyFloat Float   |
@@ -30,19 +23,40 @@ symtableInsert symbol table = table++[symbol]
 
 symtableUpdate :: Type -> [Type] -> [Type]
 symtableUpdate _ [] = fail "Not found"
-symtableUpdate (MyType a id1 es) ((MyType b id2 es2):t) =  
-                            if id1 == id2 && es == es2 then ((MyType a id1 es) : t)
-                            else (MyType b id2 es2) : symtableUpdate (MyType a id1 es) t
--- symtableUpdate (MyArray _ id1 es) ((MyArray _ id2 es2):t) =  
---                             if id1 == id2 && es == es2 then ((MyArray (_ v1) id1 es) : t)
---                             else (MyArray (_ v2) id2 es2) : symtableUpdate (MyArray (_ v1) id1 es) t
+symtableUpdate (MyType a id1 es1) ((MyType b id2 es2):t) =
+                            if id1 == id2 && es1 == es2 then ((MyType a id1 es1) : t)
+                            else (MyType b id2 es2) : symtableUpdate (MyType a id1 es1) t
+symtableUpdate (MyType a id1 es1) ((MyArray _ _ _ _):t) = symtableUpdate (MyType a id1 es1) t
+symtableUpdate (MyArray a id1 es1 b) ((MyArray c id2 es2 d):t) =
+                            if id1 == id2 && es1 == es2 then ((MyArray a id1 es1 b) : t)
+                            else (MyArray c id2 es2 d) : symtableUpdate (MyArray a id1 es1 b) t
+symtableUpdate (MyArray a id1 es1 b) ((MyType _ _ _):t) = symtableUpdate (MyArray a id1 es1 b) t
 
 symtableDelete :: String -> [Type] -> [Type]
 symtableDelete _ [] = []
 symtableDelete es1 ((MyType typex id2 es2):t) =  
                             if es1 == es2 then t
                             else (MyType typex id2 es2) : symtableDelete es1 t
+symtableDelete es1 ((MyArray a id es2 s):t) =
+                            if es1 == es2 then symtableDelete es1 t
+                            else (MyArray a id es2 s) : symtableDelete es1 t
 
 -- TODO::adicionar o token 'function' no lexer.x, criar no arquivo token.hs o Token em si, 
 --       criar no stmts.hs a estrutura da função.
 -- symtableDeleteScope :: String -> [Type] -> [Type]
+
+-- Exemplo de uso do symtableUpdate atualizando o MyType para 2
+-- symtableUpdate
+--    (MyType (MyInt 2) "nomeDaVariavel" "escopo") 
+--    ([
+--        MyArray [(MyInt 1,[0,0]),(MyInt 0,[0,1]),(MyInt 0,[1,0]),(MyInt 1,[1,1])] "nomeDaVariavel" "escopo" [2,2],
+--        MyType (MyInt 1) "nomeDaVariavel" "escopo"
+--    ])
+
+-- Exemplo de uso do symtableDelete removendo MyType 1
+-- symtableDelete
+--    "escopo"
+--    ([
+--        MyArray [(MyInt 1,[0,0]),(MyInt 0,[0,1]),(MyInt 0,[1,0]),(MyInt 1,[1,1])] "nomeDaVariavel" "escopo" [2,2],
+--        MyType (MyInt 1) "nomeDaVariavel" "escopo"
+--    ])
