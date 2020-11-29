8845f30c8c0727b3cac4e4d3284554cdde2acb5e
Ítalo Epifânio <italoepifaniols@gmail.com>
2020-11-28 23:20:08 -0300

    Falta tratar na tabela de símbolo as operações e as strings

--- src/memory.hs
+++ src/memory.hs
@@ -50,6 +50,10 @@ symtableDelete es1 ((MyArray a id es2 s):t) =
                             if es1 == es2 then symtableDelete es1 t
                             else (MyArray a id es2 s) : symtableDelete es1 t
 
+""" TODO::Da forma que tá aqui tratamos tipos simples e arrays, mas operações (exemplo 1+2+3*4) não
+são tratadas. É preciso verificar se xs != []: Se o token é um bracket chamar convertArrayStmtsToMyArray
+caso contrário tratar as operações (talvez seja preciso mudar a tipagem final para [Type]).
+"""
 fromToken :: [Token] -> String -> String -> Type
 fromToken (x:xs) nome escopo = 
     if xs == [] then fromTokenX x nome escopo          -- Modifica tipos simples: Int, String, Float
